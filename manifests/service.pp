beecac3555fb4a37ddfb58f5b1b3de2cfb8d08e7
Dmitry Sergeev <identw@gmail.com>
2021-07-21 13:08:18 +0500

    enable support tlsv1.0 and tls1.1 by default

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,16 +1,36 @@
 class nginx::service () inherits nginx {
     
     if $nginx::service_manage == true {
+
+        if $nginx::openssl_cnf {
+            file { '/etc/systemd/system/nginx.service':
+              ensure => file,
+              source => "puppet:///modules/${module_name}/nginx.service"
+            }
+
+            file {'/etc/ssl/openssl_nginx.cnf':
+              ensure => file,
+              source => "puppet:///modules/${module_name}/openssl.cnf"
+            }
+
+            exec { "systemd_daemon_reload_nginx_service":
+              command     => "systemctl daemon-reload",
+              path        => ['/bin'],
+              refreshonly => true,
+              subscribe   => File['/etc/systemd/system/nginx.service']
+            }
+        }
+
         exec {'nginx_test_config':
-            command     => 'nginx -t',
-            path        => ['/usr/sbin'],
-            refreshonly => true,
-            notify      => Service['nginx'],
+          command     => 'nginx -t',
+          path        => ['/usr/sbin'],
+          refreshonly => true,
+          notify      => Service['nginx'],
         }
         service { 'nginx':
-            ensure     => running,
-            enable     => true,
-            restart    => $reload_nginx
+          ensure     => running,
+          enable     => true,
+          restart    => $reload_nginx
         }
     }
 }
